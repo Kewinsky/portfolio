3fd97cdb1f5be1da64ad355268aec06a8a89a1cc
Kewin Tao Anh <kewin.taoanh@gmail.com>
2025-02-10 08:28:15 +0100

    remove prisma

--- src/app/api/submitContactForm/route.ts
+++ src/app/api/submitContactForm/route.ts
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-import prisma from "@/lib/prismaClient";
 import { sendEmail } from "@/lib/nodemailer";
 
 interface ContactFormRequest {
@@ -12,15 +11,6 @@ export async function POST(request: Request) {
   try {
     const { name, email, message }: ContactFormRequest = await request.json();
 
-    await prisma.contactForm.create({
-      data: {
-        name,
-        email,
-        message,
-      },
-    });
-
-    // Send the contact message to yourself via email
     await sendEmail(
       `New Contact Form Submission: ${name}`,
       `You have received a new contact form submission.\n\nName: ${name}\nEmail: ${email}\nMessage: ${message}`
